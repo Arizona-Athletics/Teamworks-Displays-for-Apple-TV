8cab43b6952566a56dde4f8bf46379574b826012
Jason Cantor <jcantor@arizona.edu>
2026-04-08 11:07:07 -0700

    Initial Commit

---
+++ TeamWorks Arizona/TeamWorks_ArizonaApp.swift
@@ -0,0 +1,17 @@
+//
+//  TeamWorks_ArizonaApp.swift
+//  TeamWorks Arizona
+//
+//  Created by Jason Cantor on 1/21/26.
+//
+
+import SwiftUI
+
+@main
+struct TeamWorks_ArizonaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TeamWorks ArizonaTests/TeamWorks_ArizonaTests.swift
@@ -0,0 +1,17 @@
+//
+//  TeamWorks_ArizonaTests.swift
+//  TeamWorks ArizonaTests
+//
+//  Created by Jason Cantor on 1/21/26.
+//
+
+import Testing
+@testable import TeamWorks_Arizona
+
+struct TeamWorks_ArizonaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
